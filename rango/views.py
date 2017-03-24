4d623983dd366c3e755c25586c87c2f14b90b1d7
stass21 <stass21@rambler.ru>
2017-03-24 23:09:38 +0300

    14

--- rango/views.py
+++ rango/views.py
@@ -9,4 +9,5 @@ def index(request):
 
 
 def about(request):
-    return  HttpResponse('Rango says here is the about page.<br> <a href=/rango>index</a>')
+    context_dict = {'wer': 'Новая картинка!!!'}
+    return  render(request, 'rango/about.html', context_dict)
