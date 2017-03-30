5b4c6931e27ed27e2397aaedc50a7d1c988a743b
stass21 <stass21@rambler.ru>
2017-03-31 00:53:53 +0300

    18

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rango.models import Category
 from django.http import HttpResponse
-
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -12,3 +12,20 @@ def index(request):
 def about(request):
     context_dict = {'wer': 'Новая картинка!!!'}
     return  render(request, 'rango/about.html', context_dict)
+
+
+def category(request,category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
