2aa6a0a7c10567a50f6e896f77f3e4c5af7f7363
stass21 <stass21@rambler.ru>
2017-04-07 00:40:22 +0300

    20

--- rango/views.py
+++ rango/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 from rango.models import Page
 
 def index(request):
-    category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'pages': page_list}
     return render(request, 'rango/index.html', context_dict)
 
 
